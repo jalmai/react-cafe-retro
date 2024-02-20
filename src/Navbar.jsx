fc476bd50c646a89902e713b6cf693398b5320bf
mingman <ingman.m@gmail.com>
2024-02-20 15:40:50 +0100

    Navbar in React

---
+++ src/Navbar.jsx
@@ -0,0 +1,32 @@
+import "./Navbar.css";
+
+export function Navbar() {
+  const links = [
+    {
+      id: 1,
+      name: "Hot",
+      path: "#hot",
+    },
+    {
+      id: 2,
+      name: "Juicy",
+      path: "#juicy",
+    },
+    {
+      id: 3,
+      name: "Cosy",
+      path: "#cosy",
+    },
+  ];
+  return (
+    <nav className="navbar">
+      {links.map((link) => {
+        return (
+          <a key={link.id} href={link.path}>
+            {link.name}
+          </a>
+        );
+      })}
+    </nav>
+  );
+}
