786fdcef146744e5f3d31f4cacb47200549a435a
mingman <ingman.m@gmail.com>
2024-02-20 17:34:14 +0100

    added box to cosy

--- src/Cosy.jsx
+++ src/Cosy.jsx
@@ -1,3 +1,4 @@
+import { Box } from "./Box.jsx";
 export function Cosy() {
   return (
     <section id="cosy">
@@ -5,18 +6,15 @@ export function Cosy() {
         <h1>Cosy</h1>
         <p className="p-1">Hang around. Enjoy the settings.</p>
         <p className="p-2">Use our fast WiFi. Borrow a newspaper or a novel.</p>
-        <div className="box">
-          <div className="row">
-            <span>Mon-Sun </span>
-            <span>8am – 11pm</span>
-          </div>
-          <div className="row">
-            <span>Caffe Retro</span> <span>Canto VI</span>
-          </div>
-          <div className="row">
-            <span>0123-45 67 89</span> <span>caffe@lorem.pge</span>
-          </div>
-        </div>
+        <Box
+          item={{
+            article: [
+              { id: 1, name: "Mon-Sun", price: "8am – 11pm" },
+              { id: 2, name: "Caffe Retro", price: "Canto VI" },
+              { id: 3, name: "0123-45 67 89", price: "caffe@lorem.pge" },
+            ],
+          }}
+        />
       </article>
     </section>
   );
